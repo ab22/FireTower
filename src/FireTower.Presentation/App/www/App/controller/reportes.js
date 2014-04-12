7bc31660fef908c203324d011e7434222f388863
Byron Sommardahl <byron@acklenavenue.com>
2014-04-12 10:21:17 -0500

    Removed html and js.

--- src/FireTower.Presentation/App/www/App/controller/reportes.js
+++ src/FireTower.Presentation/App/www/App/controller/reportes.js
@@ -35,43 +35,7 @@ angular.module('firetower')
         $scope.isValidEmail = function(email) {
             var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             return re.test(email);
-        };
-
-        $scope.sendEmail = function(date, locationDescription, latitude, longitude) {
-            $ionicPopup.prompt({
-                title: 'Enviar Correo',
-                subTitle: 'Ingrese un correo para enviar la información del incendio',
-                inputType: 'email',
-                inputPlaceholder: 'Correo'
-            }).then(function(res) {
-                if (res) {
-                    if ($scope.isValidEmail(res)) {
-                        $http.post('/SendDisasterByEmail', { Email: res, CreatedDate: date, LocationDescription: locationDescription, Latitude: latitude, Longitude: longitude }).success(function(response) {
-                            $ionicPopup.alert({
-                                title: 'Hey!',
-                                content: 'El mensaje fue enviado.'
-                            }).then(function(res) {
-
-                            });
-                        }).error(function(XMLHttpRequest, textStatus, errorThrown) {
-                            $ionicPopup.alert({
-                                title: 'Oh no!',
-                                content: 'El mensaje no puede ser enviado, pruebe mas tarde.'
-                            }).then(function(res) {
-
-                            });
-                        });
-                    } else {
-                        $ionicPopup.alert({
-                            title: 'Error de correo',
-                            content: 'La direccion de correo no es valida.'
-                        }).then(function(res) {
-
-                        });
-                    }
-                }
-            });
-        };
+        };        
 
         $scope.reportes = null;
         getAllReports();
