dc73ddc53b758604eb5a086a62e10e80e5e37869
Byron Sommardahl <byron@acklenavenue.com>
2014-04-14 17:15:03 -0500

    Adding fake photo.

--- src/FireTower.Presentation/App/www/App/controller/crear-reporte.js
+++ src/FireTower.Presentation/App/www/App/controller/crear-reporte.js
@@ -52,11 +52,13 @@
                 showBackdrop: false
             });
 
+            var photo = $scope.base64foto || "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAABGdBTUEAALGOfPtRkwAAACBjSFJNAAB6JQAAgIMAAPn/AACA6QAAdTAAAOpgAAA6mAAAF2+SX8VGAAABLElEQVR42qSTQStFURSFP7f3XygyoAwoYSYMPCIpk2egMFSmUvwCRpSRDIwYGbwyVuYykB9y914m951z7nHe6J26dc9u77XXWmdvJLF7/audqx9JYuvyW92LL0li8K2df2r17CPEVk7ftXTclyQqAMmRCwC5I3fS42a4W7y74VYDNAAuJA8AaXIsSACsDgAdAJeFrnnyoMBygKZJJ3b1It0AmsTMDPdEgrujJqHEwCxqznMaD2KgyCDRnEuo8qJhHvx/hcQDbzGoix5Yi4G1TcwZWNEDKwJU+WDkhg2ToDaD+M65YcVB8jg3Y5IY5VQAyyf9gLJw+CqAuYNnAczsPQpgevtBU937kDexcdssj8Ti0ZskMd97CRs3u//U2sjJzbtwH1+/Cf8jS/gbAMmWc42HzdIjAAAAAElFTkSuQmCC";
+        
             DisasterService.CreateDisaster({
                 LocationDescription: $scope.LocationDescription,
                 Latitude: $scope.location.latitude,
                 Longitude: $scope.location.longitude,
-                FirstImageBase64: $scope.base64foto
+                FirstImageBase64: photo
             }).success(function() {
                 showDetails();
             }).error(function() {
