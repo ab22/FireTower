018bab694958fdf6ce0ac7829f011ff91d84f514
Byron Sommardahl <byron@acklenavenue.com>
2014-04-17 12:22:43 -0500

    Caching and refreshing.

--- src/FireTower.Presentation/App/www/App/controller/reportes.js
+++ src/FireTower.Presentation/App/www/App/controller/reportes.js
@@ -1,59 +1,71 @@
 angular.module('firetower')
-    .controller('ReportesController', ['$scope', '$ionicLoading', 'data', 'Math', '$ionicPopup', '$http', '$location', function($scope, $ionicLoading, data, Math, $ionicPopup, $http, $location) {
+    .controller('ReportesController', ['cache', '$scope', '$ionicLoading', 'data', 'Math', '$ionicPopup', '$http', '$location',
+        function(cache, $scope, $ionicLoading, data, Math, $ionicPopup, $http, $location) {
 
-        $scope.viewReport = function(disasterId) {
-            $location.path("/app/reporte/" + disasterId);
-        };
+            $scope.viewReport = function(disasterId) {
+                $location.path("/app/reporte/" + disasterId);
+            };
 
-        $scope.refreshReports = function(){
-            getAllReports();
-            $scope.$broadcast('scroll.refreshComplete');
-            $scope.$broadcast('scroll.resize');
-        };
+            $scope.refreshReports = function() {
+                getAllReports();
+                $scope.$broadcast('scroll.refreshComplete');
+                $scope.$broadcast('scroll.resize');
+            };
 
-        var getAllReports = function () {
-            $scope.loadingFires = $ionicLoading.show({
-                content: 'Cargando datos de incendios...',
-                animation: 'fade-in',
-                showBackdrop: false,
-                maxWidth: 200,
-                showDelay: 500
-            });
+            var setIncendios = function(data) {
+                for (var i = 0; i < data.length; i++) {
+                    var formattedDate = moment(data[i].CreatedDate.$date).fromNow();
+                    data[i].CreatedDate.$dateformatted = formattedDate;
+                    data[i].SeverityAverage = Math.Average(data[i].SeverityVotes);
+                }
 
-            data.getAllReports()
-                .success(function(data) {
-                    $scope.reportes = data;
-                    for (var i = 0; i < $scope.reportes.length; i++) {
-                        var formattedDate = $scope.reportes[i].CreatedDate.$date;
-                        formattedDate = moment((new Date()).toLocaleDateString()).fromNow();
-                        $scope.reportes[i].CreatedDate.$dateformatted = formattedDate;
-                        $scope.reportes[i].SeverityAverage = Math.Average($scope.reportes[i].SeverityVotes);
-                    }
-
-                    $scope.loadingFires.hide();
-                })
-                .error(function() {
-                    $scope.loadingFires.hide();
-                    showMessage('Error', 'No hemos podido cargar los reportes. Estas conectado a internet?');
+                $scope.reportes = data;
+            };
+            
+            var getAllReports = function() {
+                $scope.loadingFires = $ionicLoading.show({
+                    content: 'Cargando datos de incendios...',
+                    animation: 'fade-in',
+                    showBackdrop: false,
+                    maxWidth: 200,
+                    showDelay: 500
                 });
-        };
 
-        var showMessage = function(title, message) {
-            $ionicPopup.alert({
-                title: title,
-                content: message
-            });
-        };
+                cache.get("reports").then(function (cachedData) {
+                    if (cachedData) {
+                        setIncendios(cachedData);
+                        $scope.loadingFires.hide();
+                    }
+                });
 
-        $scope.isValidEmail = function(email) {
-            var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return re.test(email);
-        };        
+                data.getAllReports()
+                    .success(function(dataFromServer) {
+                        cache.set("reports", dataFromServer);
+                        setIncendios(dataFromServer);
+                        $scope.loadingFires.hide();
+                    })
+                    .error(function() {
+                        $scope.loadingFires.hide();
+                        showMessage('Error', 'No hemos podido cargar los reportes. Estas conectado a internet?');
+                    });
+            };
 
-        $scope.reportes = null;
-        getAllReports();
+            var showMessage = function(title, message) {
+                $ionicPopup.alert({
+                    title: title,
+                    content: message
+                });
+            };
 
-        $scope.crearReporte = function() {
-            $location.path('/app/crear-reporte');
-        };
-    }]);
+            $scope.isValidEmail = function(email) {
+                var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                return re.test(email);
+            };
+
+            $scope.reportes = null;
+            getAllReports();
+
+            $scope.crearReporte = function() {
+                $location.path('/app/crear-reporte');
+            };
+        }]);
