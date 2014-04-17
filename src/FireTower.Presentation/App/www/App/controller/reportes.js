acefb0470c76c35c77c7c1ba6f2870340ca3ad9f
Byron Sommardahl <byron@acklenavenue.com>
2014-04-17 12:35:50 -0500

    Removed debugger.

--- src/FireTower.Presentation/App/www/App/controller/reportes.js
+++ src/FireTower.Presentation/App/www/App/controller/reportes.js
@@ -39,7 +39,7 @@ angular.module('firetower')
                 });
 
                 data.getAllReports()
-                    .success(function(dataFromServer) {
+                    .success(function (dataFromServer) {
                         cache.set("reports", dataFromServer);
                         setIncendios(dataFromServer);
                         $scope.loadingFires.hide();
