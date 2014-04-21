f2b3ad72cee461159155368f13cbf4cfdda9ff3f
Byron Sommardahl <byron@acklenavenue.com>
2014-04-21 17:25:58 -0500

    Removed front-end files.

--- src/FireTower.Presentation/App/www/App/controller/menu.js
+++
@@ -1,7 +0,0 @@
-﻿angular.module('firetower').controller('MenuController', ['$scope', '$location', 'userManagement', function ($scope, $location, user) {
-    
-    $scope.logout = function () {
-       user.logoutUser();
-    };
-    
-}]);
--- src/FireTower.Presentation/App/www/App/filters/reverse.js
+++
@@ -1,5 +0,0 @@
-﻿angular.module('firetower').filter('reverse', function () {
-    return function (items) {
-        return items == null ? null : items.slice().reverse();
-    };
-});
